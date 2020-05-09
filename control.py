423c2833022a3147b402344100a913bdc3d892f1
rrirr <irisalvz@gmail.com>
2020-05-09 22:01:15 +0200

    cosas del gui

--- control.py
+++ control.py
@@ -14,8 +14,10 @@ class Control:
     dst_udp_port = None
     dst_tcp_port = None
 
-    # TODO variables control call
-
+    # TODO check this
+    pause_event = None
+    end_event = None
+    flag_en_llamada = False
 
     # Constantes
     max_conexiones = 10
@@ -43,22 +45,28 @@ class Control:
 
     # Funciones que envian mensajes de control
     def calling(self, nick, dst_ip, dst_port):
+        self.gui.app.setStatusbar("Llamando a {}...".format(nick), 0)
         msg = "CALLING {} {}".format(nick, self.udp_port)
         self.send_msg(msg, dst_ip, dst_port)
 
     def call_hold(self, nick, dst_ip, dst_port):
         msg = "CALL_HOLD {}".format(nick)
         self.send_msg(msg, dst_ip, dst_port)
+        self.gui.app.setStatusbar("Llamada pausada.", 0)
+        self.pause_event.set()
 
     def call_resume(self, nick, dst_ip, dst_port):
         msg = "CALL_RESUME {}".format(nick)
         self.send_msg(msg, dst_ip, dst_port)
+        self.gui.app.setStatusbar("En llamada.", 0)
+        self.pause_event.clear()
 
     def call_end(self, nick, dst_ip, dst_port):
         msg = "CALL_END {}".format(nick)
         self.send_msg(msg, dst_ip, dst_port)
-
-
+        self.gui.app.setStatusbar("Llamada finalizada.", 0)
+        self.end_event.set()
+        self.flag_en_llamada = False
 
     # Funciones para enviar mensajes de respuesta a CALLING
     def call_accepted(self, nick, dst_ip, dst_port):
@@ -73,53 +81,71 @@ class Control:
         msg = "CALL_BUSY"
         self.send_msg(msg, dst_ip, dst_port)
 
-
-
     # Funciones para realizar las acciones necesarias tras procesar mensaje
     def calling_handler(self, nick, dst_udp_port):
 
         user_info = Users_descubrimiento.query(nick)
         dst_ip = user_info[1]
         dst_port = user_info[2]
-        protocolos = user_info[3] # TODO que hacer con protocolos
+        protocolos = user_info[3]  # TODO que hacer con protocolos
 
-        # TODO cosas de la gui
-        if no busy (mirar gui):
+        if not self.flag_en_llamada:
 
-            # Si quiere aceptar
-            self.call_accepted(nick, dst_ip, dst_port)
-            self.dst_udp_port = dst_udp_port
+            res = self.gui.app.yesNoBox("Llamada entrante", nick)
+            if res:
+                self.call_accepted(nick, dst_ip, dst_port)
+                self.dst_udp_port = dst_udp_port
+                self.dst_tcp_port = dst_port
+                self.flag_en_llamada = True
+                self.gui.app.setStatusbar("En llamada.", 0)
 
+                # TODO threads, video etc
 
-            # Si no
-            self.call_denied(nick, dst_ip, dst_port)
+            else:
+                self.call_denied(nick, dst_ip, dst_port)
 
         else:
             self.call_busy(dst_ip, dst_port)
 
-    # TODO cosas de la gui
     def call_hold_handler(self, nick):
+        if not self.flag_en_llamada:
+            self.gui.app.setStatusbar("Llamada pausada.", 0)
+            self.pause_event.set()
 
     def call_resume_handler(self, nick):
+        if not self.flag_en_llamada:
+            self.gui.app.setStatusbar("En llamada.", 0)
+            self.pause_event.clear()
 
     def call_end_handler(self, nick):
-        self.dst_tcp_port = None
-        self.dst_udp_port = None
+        if not self.flag_en_llamada:
+            self.dst_tcp_port = None
+            self.dst_udp_port = None
 
-
+            self.gui.app.setStatusbar("Llamada finalizada.", 0)
+            self.end_event.set()
+            self.flag_en_llamada = False
 
     def call_accepted_handler(self, nick, dst_udp_port):
-        user_info = Users_descubrimiento.query(nick)
-        dst_ip = user_info[1]
-        self.dst_tcp_prot = user_info[2]
-        self.dst_udp_port = dst_udp_port
+        if not self.flag_en_llamada:
+            user_info = Users_descubrimiento.query(nick)
+            dst_ip = user_info[1]
+            self.dst_tcp_port = user_info[2]
+            self.dst_udp_port = dst_udp_port
+            self.flag_en_llamada = True
+
+            self.gui.app.infoBox("Informacinó de llamada", "{} ha aceptado tu llamada.".format(nick))
+            self.gui.app.setStatusbar("En llamada.", 0)
+
+            # TODO threads, video etc
 
     def call_denied_handler(self, nick):
+        self.gui.app.infoBox("Informacinó de llamada", "{} no ha aceptado tu llamada.".format(nick))
+        self.gui.app.setStatusbar("", 0)
 
     def call_busy_handler(self):
-
-
-
+        self.gui.app.infoBox("Informacinó de llamada", "El usuario se encuentra en una llamada.")
+        self.gui.app.setStatusbar("", 0)
 
     # Funciones para recibir y procesar peticiones
     def listening(self):
